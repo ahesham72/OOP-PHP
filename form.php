53a45d4375590a7da728272a74bbfe18e7043f83
ahmed <ahesham72@gmail.com>
2020-06-12 23:42:42 +0200

    create frist class

---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<form action="calc.php" method='Get'>
+
+    <input  type='number' name='num1' placeholder='Enter Number'>
+    <input  type='number' name='num2' placeholder='Enter Number'>
+    <select name="op" id="">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+
+<br/>
+<input type="submit" value='Calcluate'>
+</form>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+// class student
+// {
+//     public $name;
+//     public $id;
+//     public $level;
+
+//     // method
+//     public function registerOnCourse($courseName)
+//     {
+//         echo "Welcome to " . $courseName . "<br>";
+//     }
+//     public function leaveCourse($courseName)
+//     {
+//         echo "by Form " . $courseName . "<br>";
+//     }
+// }
+
+// $newStudent = new student();
+
+
+// $newStudent->name="ali";
+// $newStudent->id='22';
+// $newStudent->level="3333";
+// $newStudent->registerOnCourse("JavaScript");
+
+// echo "Name :" . $newStudent->name . "<br>";
+// echo "ID   :" . $newStudent->id   . "<br>";
+// echo "Level:" . $newStudent->level. "<br>";
+
+// $newStudent->leaveCourse("JavaScript");
+
+
+
+class AppleDevice {
+
+    
+
+}
+
+$iphone6plus = new AppleDevice();
+
+echo '<pre>'; 
+
+var_dump($iphone6plus);
+echo '</pre>'; 
+
+
+
+
+
+
+
+
+
+
